024ad207078a316a0de20c9e7cfd2528f4e71347
billie-drimaes <billie@drimaes.com>
2022-05-19 18:20:18 +0900

    스프링 시큐리티 적용 후 테스트코드 에러 해결

--- src/main/java/com/pongi/project/springboot/Application.java
+++ src/main/java/com/pongi/project/springboot/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import  org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing  //WebMvcTest는 entity클래스가 없어 에러 발생 => SpringBootApplication과 분리
+// WebMvcTest는 일반적인 configuration을 스캔하지 않기 때문에 JpaConfig파일로 분리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/pongi/project/springboot/web/HelloControllerTest.java
+++ src/test/java/com/pongi/project/springboot/web/HelloControllerTest.java
@@ -3,12 +3,16 @@ package com.pongi.project.springboot.web;
 //Junit 4 -> 5로 업데이트 되면서 라이브러리 사용법 변경 (주석참고)
 
 //Junit 4: import org.junit.Test;
+import com.pongi.project.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 //Junit 4: import org.junit.runner.RunWith;
 import org.junit.jupiter.api.extension.ExtendWith; //junit과 springboot test 간의 연결자 역할
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 //import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,13 +22,19 @@ import static org.hamcrest.Matchers.is;
 
 //Junit 4: @RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class) // web에 집중하는 스프링 어노테이션(controller 사용 시 사용)
+@WebMvcTest(// web에 집중하는 스프링 어노테이션(controller 사용 시 사용)
+        controllers = HelloController.class,
+        //WebMvcTest는 WebMvcConfigurer, controller 등을 읽어오지만, 그외에 repository,service,component는 스캔대상이 아니어서, SecurityConfig는 읽어오지만, CustomOAuth2UserService는 읽지못함
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class) //webMvcTest에서 SecurityConfig를 스캔하지 않도록 설정
+        })
 public class HelloControllerTest {
 
     @Autowired //Bean 주입
     private MockMvc mvc; //스프링 웹 api test의 시작점
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_return된다() throws Exception{
         String hello = "hello";
 
@@ -34,6 +44,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto_return() throws  Exception{
         String name = "hello";
         int amount = 1000;
